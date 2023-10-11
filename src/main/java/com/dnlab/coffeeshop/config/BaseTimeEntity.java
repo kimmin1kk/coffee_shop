c0067fc386dda89d0773e79d2633056280950f89
kimmin1kk <rudals1888@naver.com>
2023-10-11 13:01:13 +0900

    feat: BaseTimeEntity 추가

---
+++ src/main/java/com/dnlab/coffeeshop/config/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.dnlab.coffeeshop.config;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private Timestamp createdDate;
+
+    @LastModifiedDate
+    private Timestamp modifiedDate;
+}
